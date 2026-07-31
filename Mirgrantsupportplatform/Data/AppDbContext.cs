beab1bb4e7fbd7c886b8fd8bb8df46722a5cc43f
Liamhpw <liamhorganpw@gmail.com>
2026-07-31 16:04:59 +1200

    MSP-6 Choose Database

---
+++ Mirgrantsupportplatform/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Mirgrantsupportplatform.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebAppDatabase"));
+        }
+
+        /*
+         * author: Liam Horgan
+         This will be where we can set up the tables in the database. For example, if we have a table called "Users", we would add a DbSet for it here:
+         public DbSet<User> Users { get; set; }
+         Will create the classes to build up the database once we know what we want to store in the database.
+         */
+    }
+}
